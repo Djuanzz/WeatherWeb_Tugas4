711b02c73ce20d47ce67c1234fef29d94f97884e
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2023-10-09 23:31:37 +0700

    feat: using fetch api

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ let namaKota = document.querySelector(".nama-kota");
 let suhu = document.querySelector(".suhu");
 let kondisi = document.querySelector(".kondisi");
 let gambar = document.querySelector(".gambar");
+let apiKey = "5df066624a2b37618a4e6f238f45c100";
 
 const dataCuaca = new Set(["Clear", "Clouds", "Haze", "Snow", "Rain"]);
 
@@ -11,36 +12,61 @@ function getInput() {
   return inputan.value;
 }
 
-function loadDoc() {
-  const xhttp = new XMLHttpRequest();
-  let location = getInput();
-  let apiKey = "5df066624a2b37618a4e6f238f45c100";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+async function getWeather() {
+  try {
+    let location = getInput();
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+    const res = await fetch(apiUrl);
+    const data = await res.json();
 
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      let response = JSON.parse(this.responseText);
+    console.log(data);
 
-      if (response.cod === 200) {
-        let cuaca = response.weather[0].main;
-        namaKota.innerHTML = location;
-        suhu.innerHTML = `${Math.floor(response.main.temp - 273)}°C`;
-        kondisi.innerHTML = cuaca;
+    const cuaca = data.weather[0].main;
+    namaKota.innerHTML = data.name;
+    // console.log(data.name);
+    suhu.innerHTML = `${Math.floor(data.main.temp - 273)}°C`;
+    kondisi.innerHTML = cuaca;
 
-        if (dataCuaca.has(cuaca)) {
-          gambar.src = `./img/${cuaca}.png`;
-        } else {
-          gambar.src = "./img/space1.jpg";
-        }
-        // console.log(response);
-      }
-    } else {
-      namaKota.innerHTML = "Lokasi tidak ditemukan";
-      suhu.innerHTML = "Error";
-      kondisi.innerHTML = "Error";
-    }
-  };
+    gambar.setAttribute("src", `./img/${cuaca}.png`);
+  } catch (err) {
+    console.log(err);
 
-  xhttp.open("GET", apiUrl);
-  xhttp.send();
+    gambar.setAttribute("src", `./img/space1.jpg`);
+    namaKota.innerHTML = "Lokasi tidak ditemukan";
+    suhu.innerHTML = "Error";
+    kondisi.innerHTML = "Error";
+  }
 }
+
+// function loadDoc() {
+//   const xhttp = new XMLHttpRequest();
+//   let location = getInput();
+//   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}`;
+
+//   xhttp.onreadystatechange = function () {
+//     if (this.readyState == 4 && this.status == 200) {
+//       let response = JSON.parse(this.responseText);
+
+//       if (response.cod === 200) {
+//         let cuaca = response.weather[0].main;
+//         namaKota.innerHTML = location;
+//         suhu.innerHTML = `${Math.floor(response.main.temp - 273)}°C`;
+//         kondisi.innerHTML = cuaca;
+
+//         if (dataCuaca.has(cuaca)) {
+//           gambar.src = `./img/${cuaca}.png`;
+//         } else {
+//           gambar.src = "./img/space1.jpg";
+//         }
+//         // console.log(response);
+//       }
+//     } else {
+//       namaKota.innerHTML = "Lokasi tidak ditemukan";
+//       suhu.innerHTML = "Error";
+//       kondisi.innerHTML = "Error";
+//     }
+//   };
+
+//   xhttp.open("GET", apiUrl);
+//   xhttp.send();
+// }
